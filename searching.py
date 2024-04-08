7b4c683d7944d0f2d15299d31395ab1900a44dfd
Veronika Sromova <237011@vut.cz>
2024-04-08 09:02:25 +0200

    searching

--- searching.py
+++ searching.py
@@ -1,8 +1,8 @@
 import os
+import json
 
 # get current working directory path
 cwd_path = os.getcwd()
-
 
 def read_data(file_name, field):
     """
@@ -13,10 +13,27 @@ def read_data(file_name, field):
     """
     file_path = os.path.join(cwd_path, file_name)
 
+    # Nacteni povolenych klicu ze souboru
+    with open("sequential.json", mode="r") as file:
+        allowed_key = json.load(file)
+
+    # Overeni, zda je zadany klic v mnozine povolenych klicu
+    if field not in allowed_key:
+        return None
+
+    with open(file_name, mode="r") as f:
+        data = json.load(f)
+
+    # Vraceni hodnot
+    return data.get(field)
 
 def main():
-    pass
+    # pass
+    # Zavolani funkce read_data s pozadovanymi vstupy
+    sequential_data = read_data("sequential.json", "unordered_numbers")
 
+    # Vytiskni obsah promenne sequential_data
+    print(sequential_data)
 
 if __name__ == '__main__':
     main()
