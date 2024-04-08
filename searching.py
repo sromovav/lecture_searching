912134178b1de024b6d6ff61da8117cf6a2d471d
Veronika Sromova <237011@vut.cz>
2024-04-08 10:21:34 +0200

    searching2

--- searching.py
+++ searching.py
@@ -1,5 +1,6 @@
 import os
 import json
+from collections import Counter
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -27,6 +28,44 @@ def read_data(file_name, field):
     # Vraceni hodnot
     return data.get(field)
 
+def linear_search(sequence, number):
+    """
+    :param sequence:
+    :param number:
+    :return:
+    """
+    positions = []
+    count = 0
+
+    for i, num in enumerate(sequence):
+        if num == number:
+            positions.append(i+1)
+            count += 1
+
+    return {"positions": positions, "count": count}
+
+def pattern_search(sequence, pattern):
+    """
+    :param sequence:
+    :param pattern:
+    :return:
+    """
+    positions = set()
+    pattern_length = len(pattern)
+    sequence_length = len(sequence)
+
+    idx = 0
+
+    # for i in range(sequence_length - pattern_length +1):
+    #     if sequence[i:i+pattern_length] == pattern:
+    #         positions.add(i)
+
+
+
+
+
+    return positions
+
 def main():
     # pass
     # Zavolani funkce read_data s pozadovanymi vstupy
@@ -34,6 +73,15 @@ def main():
 
     # Vytiskni obsah promenne sequential_data
     print(sequential_data)
+
+    number = 5
+    result = linear_search(sequential_data, number)
+    print(result)
+
+    sequence = read_data("sequential.json", "dna_sequence")
+    pattern = "ATA"
+    result2 = pattern_search(sequence, pattern)
+    print(result2)
 
 if __name__ == '__main__':
     main()
